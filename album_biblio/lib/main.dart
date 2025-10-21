c14ef288022ee3aaccdd7d871502de30cb816259
Elquedeque <fasf040412@gmail.com>
2025-10-21 08:52:56 -0700

    Actualiza

--- album_biblio/lib/main.dart
+++ album_biblio/lib/main.dart
@@ -85,38 +85,89 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: Center ( child: GestureDetector(
+          child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+            const SizedBox(height: 40),
+            // Imagen
+            CircleAvatar(
+            radius: 64,
+            backgroundImage: AssetImage('assets/images/profile.jpg'),
+            ),
+            // Nombre
+            const Text(
+            'Francisco Salazar Figueroa',
+            style: TextStyle(
+            fontSize: 26,
+            fontWeight: FontWeight.bold,
+            shadows: [
+            Shadow(
+            offset: Offset(1.2, 1.2),
+            blurRadius: 1.5,
+            color: Color.fromARGB(255, 43, 53, 46),
+            ),
+            ],
+            ),
+            ),
+            const SizedBox(height: 40),
+            //Descripcion
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 28.0),
+              child: Text(
+              'Estudiante de Ingenieria en Informatica cursando el 7tmo semestre en el TecNM campus Hermosillo',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+              fontSize: 18,
+              color: Color.fromARGB(255, 0, 0, 0),
+              shadows: [
+              Shadow(
+              offset: Offset(1, 1),
+              blurRadius: 1,
+              color: Color.fromARGB(255, 40, 46, 42),
+              ),
+              ],
+              ),
+              ),
+              ),
+              const SizedBox(height: 40),
+              //Datos Generales
+              Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 28.0, vertical: 6.0),
+              child: Container(
+              padding: const EdgeInsets.all(12),
+              decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(8),
+              border: Border.all(color: Colors.green.shade700, width: 1),
+              ),
+              child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+              Text(
+              'Email: l22330883@hermosillo.tecnm.mx',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 15, color: Colors.black),
+              ),
+              SizedBox(height: 6),
+              Text(
+              'Teléfono: +52 637 103 7897',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 15, color: Colors.black),
+              ),
+              SizedBox(height: 6),
+              Text(
+              'LinkedIn: www.linkedin.com/in/fasf0412',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 15, color: Colors.black),
+              ),
+            ],
+            ) ,
+            ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
+  ),),)
+  ,);}}
